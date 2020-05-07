7087c92115b438bd1ce1701e02ccd883b1b6f57c
caicandong <1290147055@qq.com>
2020-05-08 07:12:37 +0800

    修复7好打卡bug

--- src/1-100/day29/main.cpp
+++ src/1-100/day29/main.cpp
@@ -12,9 +12,9 @@ int longest(int *array, int length, int target) {
         while (array[j] == array[i]) {
             sum += array[j];
             j++;
-        }
-        if (sum == target && j - i > ret) {
-            ret = j - i;
+            if (sum == target && j - i > ret) {
+                ret = j - i;
+            }
         }
         i = j;
     }
@@ -26,13 +26,17 @@ int main() {
 //    int length=8;
 //    int s=3;
     //答案:3
+   int A[]={1,1,2,1,1,1,2,1};
+   int length=8;
+   int s=2;
+    //答案:2
 //    int A[]={1,3,4,5,1,3,2,1};
 //    int length=8;
 //    int s=6;
     // 答案:0
-    int A[]={1};
-    int length=1;
-    int s=1;
+    // int A[]={1};
+    // int length=1;
+    // int s=1;
     //答案：1
     int ret=longest(A,length,s);
     printf("%d",ret);
